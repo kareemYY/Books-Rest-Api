314719e949e72e7828f6a57e1bb043fa1269d9dc
Kareem Yasser <kareemyasser264@gmail.com>
2026-04-14 19:54:03 +0200

    Adding Service package

--- src/main/java/com/luv2code/books/controller/BookController.java
+++ src/main/java/com/luv2code/books/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.luv2code.books.controller;
 
 import com.luv2code.books.entity.Book;
+import com.luv2code.books.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,26 +12,12 @@ import java.util.List;
 @RestController
 public class BookController {
 
-    private final List<Book> books = new ArrayList<>();
-
-
-    public BookController() {
-        initializeBooks();
-    }
-    private void initializeBooks(){
-        books.addAll(List.of(
-                new Book ("Title one","Author one" , "Science"),
-                new Book("Title two","Author Two","Science"),
-                new Book("Title three","Author Three","history"),
-                new Book ("Title four","Author four" , "Science"),
-                new Book("Title five","Author five","math"),
-                new Book("Title six","Author six","math")
-        ));
-    }
+    @Autowired
+    private BookService bookService;
 
     @GetMapping("/api")
     public List<Book> getBooks() {
-        return books;
+        return bookService.initializeBooks();
     }
 
 }
---
+++ src/main/java/com/luv2code/books/service/BookService.java
@@ -0,0 +1,28 @@
+package com.luv2code.books.service;
+
+import com.luv2code.books.entity.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class BookService {
+
+
+    private final List<Book> books= new ArrayList<>();
+
+    public List<Book> initializeBooks(){
+        books.addAll(List.of(
+                new Book ("Title one","Author one" , "Science"),
+                new Book("Title two","Author Two","Science"),
+                new Book("Title three","Author Three","history"),
+                new Book ("Title four","Author four" , "Science"),
+                new Book("Title five","Author five","math"),
+                new Book("Title six","Author six","math")
+        ));
+        return books;
+    }
+}
