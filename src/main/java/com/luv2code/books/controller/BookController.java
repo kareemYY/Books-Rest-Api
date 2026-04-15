85ff1e669ea05e5e38659cc1ba967ad8aec48b80
Kareem YAsser <kareemyasser264@gmail.com>
2026-04-15 09:26:54 +0200

    adding service package and test

--- src/main/java/com/luv2code/books/controller/BookController.java
+++ src/main/java/com/luv2code/books/controller/BookController.java
@@ -1,35 +1,23 @@
 package com.luv2code.books.controller;
 
 import com.luv2code.books.entity.Book;
+import com.luv2code.books.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class BookController {
 
-    private final List<Book> books = new ArrayList<>();
+    @Autowired
+    private BookService bookService;
 
 
-    public BookController() {
-        initializeBooks();
-    }
-    private void initializeBooks(){
-        books.addAll(List.of(
-                new Book ("Title one","Author one" , "Science"),
-                new Book("Title two","Author Two","Science"),
-                new Book("Title three","Author Three","history"),
-                new Book ("Title four","Author four" , "Science"),
-                new Book("Title five","Author five","math"),
-                new Book("Title six","Author six","math")
-        ));
-    }
 
     @GetMapping("/api")
     public List<Book> getBooks() {
-        return books;
+        return bookService.initializeBooks();
     }
 
 }
