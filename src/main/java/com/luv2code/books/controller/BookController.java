967b359f94a8291b71e7a141c8a591a00292ee3c
Kareem Yasser <kareemyasser264@gmail.com>
2026-04-19 22:31:29 +0200

    adding swagger tag and operation

--- src/main/java/com/luv2code/books/controller/BookController.java
+++ src/main/java/com/luv2code/books/controller/BookController.java
@@ -3,6 +3,8 @@ package com.luv2code.books.controller;
 import com.luv2code.books.dto.BookDto;
 import com.luv2code.books.entity.Book;
 import com.luv2code.books.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@Tag(name = "Books Rest API Endpoints" ,description = "Operations related to books")
 @RestController
 @RequestMapping("api/books")
 public class BookController {
@@ -20,6 +24,7 @@ public class BookController {
 
 
 
+    @Operation(summary = "Get All Books " , description = "Retrieve a list of available books")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<Book> getBooks(@RequestParam(required = false) String category) {
@@ -31,6 +36,7 @@ public class BookController {
 
     }
 
+    @Operation(summary = "Get A book By Id ", description = "Retrieve a specific book by id")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     public Book getBookByTitle(@PathVariable @Min(value = 1) long id) {
@@ -38,6 +44,7 @@ public class BookController {
     }
 
 
+    @Operation(summary = "Create A new Book ", description = "Add a new book to the list")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Book createBook( @Valid @RequestBody BookDto newBookDto){
@@ -45,6 +52,7 @@ public class BookController {
     }
 
 
+    @Operation(summary = "Update a book ", description = "Update the details of an book ")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
     public BookDto updateBook(@PathVariable @Min(value = 1) long id,@Valid @RequestBody BookDto updateBookDto){
@@ -53,6 +61,7 @@ public class BookController {
 
 
 
+    @Operation(summary = "delete a book ", description = "delete a book from the list")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable @Min(value = 1) long id){
