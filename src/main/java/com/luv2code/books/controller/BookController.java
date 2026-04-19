87ba95c051982646510e284f718ccb265f4b614c
Kareem Yasser <kareemyasser264@gmail.com>
2026-04-19 22:00:52 +0200

    Adding Validation And Status

--- src/main/java/com/luv2code/books/controller/BookController.java
+++ src/main/java/com/luv2code/books/controller/BookController.java
@@ -3,7 +3,10 @@ package com.luv2code.books.controller;
 import com.luv2code.books.dto.BookDto;
 import com.luv2code.books.entity.Book;
 import com.luv2code.books.service.BookService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +20,7 @@ public class BookController {
 
 
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<Book> getBooks(@RequestParam(required = false) String category) {
         if(category == null){return bookService.getAllBooks();}
@@ -27,26 +31,31 @@ public class BookController {
 
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public Book getBookByTitle(@PathVariable long id) {
+    public Book getBookByTitle(@PathVariable @Min(value = 1) long id) {
         return bookService.getBookById(id);
     }
 
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public Book createBook(@RequestBody BookDto newBookDto){
+    public Book createBook( @Valid @RequestBody BookDto newBookDto){
          return bookService.createBook(newBookDto   );
     }
 
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    public BookDto updateBook(@PathVariable long id, @RequestBody BookDto updateBookDto){
+    public BookDto updateBook(@PathVariable @Min(value = 1) long id,@Valid @RequestBody BookDto updateBookDto){
         return bookService.updateBook(id,updateBookDto);
     }
 
 
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void deleteBook(@PathVariable long id){
+    public void deleteBook(@PathVariable @Min(value = 1) long id){
         bookService.deleteBook(id);
     }
 
