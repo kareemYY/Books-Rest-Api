e15983ce1b9e7c9f001bfbfe8cedd066378a9a55
Kareem YAsser <kareemyasser264@gmail.com>
2026-05-07 14:23:02 +0300

    adding jwtService

--- src/main/java/com/luv2code/books/service/security/JwtServiceImpl.java
+++ src/main/java/com/luv2code/books/service/security/JwtServiceImpl.java
@@ -1,22 +1,65 @@
 package com.luv2code.books.service.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
 import java.util.Map;
 
+
+@Server
 public class JwtServiceImpl implements JwtService {
+
+
+    @Value("${spring.jwt.secret}")
+    private String SECRET_KEY;
+
+    @Value("${spring.jwt.expiration}")
+    private long JWT_EXPIRATION_TIME;
+
+
+
     @Override
     public String extractUsername(String token) {
-        return "";
+        return extractAllClaims(token).getSubject();
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     @Override
     public boolean isTokenValid(String token, UserDetails userDetails) {
+
         return false;
     }
 
     @Override
     public String generateToken(Map<String, Object> claims, UserDetails userDetails) {
-        return "";
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+JWT_EXPIRATION_TIME))
+                .signWith(getSignKey(),Jwts.SIG.HS256)
+                .compact();
     }
+
+    private SecretKey getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+
+    }
+
+
 }
